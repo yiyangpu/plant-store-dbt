21873e88fca08d4c129a4949456eb4ededa789c5
Ruoyan Ma <yuki96495@gmail.com>
2026-04-16 18:49:47 +0000

    changes with invalid identifiers

--- models/base/base_web_item_views.sql
+++ models/base/base_web_item_views.sql
@@ -1,11 +1,11 @@
 SELECT
-    _fivetran_id,
+    "_fivetran_id" AS _FIVETRAN_ID,
     SESSION_ID,
     ITEM_NAME,
     ITEM_VIEW_AT AS ITEM_VIEW_TIME,
     PRICE_PER_UNIT,
     ADD_TO_CART_QUANTITY,
     REMOVE_FROM_CART_QUANTITY,
-    _FIVETRAN_DELETED,
-    _FIVETRAN_SYNCED
+    "_fivetran_deleted" AS _FIVETRAN_DELETED,
+    "_fivetran_synced" AS _FIVETRAN_SYNCED
 FROM {{ source('web', 'item_views') }}
