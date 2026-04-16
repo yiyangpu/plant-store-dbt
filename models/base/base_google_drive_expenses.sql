5481e6da33522de94a08fadf3e774c6812feb8ba
Ruoyan Ma <yuki96495@gmail.com>
2026-04-16 03:06:10 +0000

    Specify the column name

--- models/base/base_google_drive_expenses.sql
+++ models/base/base_google_drive_expenses.sql
@@ -1,8 +1,5 @@
 SELECT
     DATE AS expense_date,
-
     TO_NUMBER(REGEXP_REPLACE(EXPENSE_AMOUNT, '[^0-9.]', '')) AS expense_amount,
-
     EXPENSE_TYPE
-
 FROM {{ source('google_drive', 'expenses') }}
--- models/base/base_google_drive_returns.sql
+++ models/base/base_google_drive_returns.sql
@@ -1,5 +1,5 @@
 SELECT
     ORDER_ID,
-    RETURNED_AT,
+    RETURNED_AT AS RETURNED_DATE,
     IS_REFUNDED
 FROM {{ source('google_drive', 'returns') }}
