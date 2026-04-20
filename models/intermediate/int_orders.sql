0f701964b307362037e0221d8d45d9cdc28b744a
AO LI <lala125426@gmail.com>
2026-04-20 04:41:00 +0000

    build int_employee and int_orders models and add schema documentation

--- models/intermediate/int_orders.sql
+++ models/intermediate/int_orders.sql
@@ -1,61 +1,38 @@
-with orders as (
-
-    select *
-    from {{ ref('base_web_orders') }}
-
+WITH orders AS (
+    SELECT *
+    FROM {{ ref('base_web_orders') }}
 ),
 
-returns as (
-
-    select *
-    from {{ ref('base_google_drive_returns') }}
-
+returns AS (
+    SELECT *
+    FROM {{ ref('base_google_drive_returns') }}
 ),
 
-dedup_orders as (
-
-    select *
-    from (
-        select *,
-               row_number() over (
-                   partition by order_id
-                   order by order_time desc
-               ) as rn
-        from orders
+dedup_orders AS (
+    SELECT *
+    FROM (
+        SELECT *,
+               ROW_NUMBER() OVER (
+                   PARTITION BY order_id
+                   ORDER BY order_time DESC
+               ) AS rn
+        FROM orders
     )
-    where rn = 1
-
+    WHERE rn = 1
 ),
 
-joined as (
-
-    select
+final AS (
+    SELECT
         o.order_id,
         o.order_time,
-        o.price_per_unit,
-        o.add_to_cart_quantity,
-        r.is_refunded
-
-    from dedup_orders o
-    left join returns r
-        on o.order_id = r.order_id
-
-),
-
-final as (
-
-    select
-        order_id,
-        order_time,
-        is_refunded,
-
-        case
-            when is_refunded = true then 0
-            else price_per_unit * add_to_cart_quantity
-        end as revenue
-
-    from joined
-
+        r.is_refunded,
+        CASE
+            WHEN r.is_refunded = TRUE THEN TRUE
+            ELSE FALSE
+        END AS is_refunded_bool
+    FROM dedup_orders o
+    LEFT JOIN returns r
+        ON o.order_id = r.order_id
 )
 
-select * from final
+SELECT * FROM final
