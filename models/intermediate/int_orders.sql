670410f631b4b8579c0ff6c9cc4a4c98e5a39c2b
AO LI <lala125426@gmail.com>
2026-04-21 14:53:40 +0000

    fix int_orders model

--- models/intermediate/int_orders.sql
+++ models/intermediate/int_orders.sql
@@ -1,38 +1,45 @@
-WITH orders AS (
-    SELECT *
-    FROM {{ ref('base_web_orders') }}
-),
+WITH dedup_orders AS (
 
-returns AS (
-    SELECT *
-    FROM {{ ref('base_google_drive_returns') }}
-),
-
-dedup_orders AS (
     SELECT *
     FROM (
         SELECT *,
-               ROW_NUMBER() OVER (
-                   PARTITION BY order_id
-                   ORDER BY order_time DESC
-               ) AS rn
-        FROM orders
+            ROW_NUMBER() OVER (
+                PARTITION BY order_id
+                ORDER BY order_time DESC
+            ) AS rn
+        FROM {{ ref('base_web_orders') }}
     )
     WHERE rn = 1
+
+),
+
+returns_cleaned AS (
+
+    SELECT
+        order_id,
+        MAX(
+            CASE
+                WHEN is_refunded = TRUE THEN TRUE
+                ELSE FALSE
+            END
+        ) AS is_refunded_bool,
+        MAX(is_refunded) AS is_refunded
+    FROM {{ ref('base_google_drive_returns') }}
+    GROUP BY order_id
+
 ),
 
 final AS (
+
     SELECT
         o.order_id,
         o.order_time,
         r.is_refunded,
-        CASE
-            WHEN r.is_refunded = TRUE THEN TRUE
-            ELSE FALSE
-        END AS is_refunded_bool
+        COALESCE(r.is_refunded_bool, FALSE) AS is_refunded_bool
     FROM dedup_orders o
-    LEFT JOIN returns r
+    LEFT JOIN returns_cleaned r
         ON o.order_id = r.order_id
+
 )
 
 SELECT * FROM final
