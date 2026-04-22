7e30845418c928a18dd11684cf6d95c8e1db0aef
Ruoyan Ma <yuki96495@gmail.com>
2026-04-22 21:19:35 +0000

    small changes with TRIM

--- models/intermediate/int_expenses.sql
+++ models/intermediate/int_expenses.sql
@@ -1,7 +1,7 @@
 WITH cleaned_expenses AS (
     SELECT
         DATE(EXPENSE_DATE) AS expense_date,
-        LOWER(EXPENSE_TYPE) AS expense_type,
+        LOWER(TRIM(EXPENSE_TYPE)) AS expense_type,
         EXPENSE_AMOUNT
     FROM {{ ref('base_google_drive_expenses') }}
 )
