c5ff9b13d2ad92a68b7882aef24922248f988899
Ruoyan Ma <yuki96495@gmail.com>
2026-04-22 18:49:29 +0000

    delete conversion rate

--- models/intermediate/int_item.sql
+++ models/intermediate/int_item.sql
@@ -4,8 +4,5 @@ SELECT
     COUNT(*) AS total_item_views,
     SUM(add_to_cart_quantity) AS total_add_to_cart,
     SUM(remove_from_cart_quantity) AS total_remove_from_cart,
-
-    COUNT(DISTINCT CASE WHEN add_to_cart_quantity > 0 THEN session_id END) * 1.0 / COUNT(*) AS conversion_rate,
-
 FROM {{ ref('base_web_item_views') }}
 GROUP BY item_name
